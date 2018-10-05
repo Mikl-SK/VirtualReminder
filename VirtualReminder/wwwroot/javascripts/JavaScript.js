44827f28c2548e67a646e831426f332c2c04c7f5
Minerr <jack.corp@hotmail.com>
2018-10-05 05:58:18 +0200

    Added popup for AddReminder

---
+++ VirtualReminder/wwwroot/javascripts/JavaScript.js
@@ -0,0 +1,8 @@
+﻿
+function ShowAddReminderMenu() {
+	document.querySelector(".popup").style.visibility = "visible";
+}
+
+function CloseAddReminderMenu() {
+	document.querySelector(".popup").style.visibility = "hidden";
+}
